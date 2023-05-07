3db76861bfac5981200863bf370bdf0eb6791d94
taimoorahmed91 <taimoorahmed91@gmail.com>
2023-05-07 19:29:11 +0000

    policy set authen post - put working separete button

--- landscape/run_replicateauthentication_put.php
+++ landscape/run_replicateauthentication_put.php
@@ -7,9 +7,6 @@
 
 
 
-  header( "refresh:2;url=http://10.48.30.213/landscape/replicateauthentication.php" );
-        //Assign get variable
-        $id = $_GET['id'];
 
 
 
@@ -33,6 +30,41 @@
 system("sudo -S python3 /root/ise-landscape/policyset/insert_dstpolicysetid.py  $id  $dstise $dstpolicyset");
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+  $query ="SELECT replicateauthentication.id, replicateauthentication.dstise, replicateauthentication.dstpolicysetid, replicateauthentication.dstpolicyid, replicateauthentication.sourceauthentication, authentications.authenticationname, authentications.authenticationid,replicateauthentication.code_post, replicateauthentication.code_put, replicateauthentication.time from replicateauthentication INNER JOIN authentications ON authentications.id = replicateauthentication.sourceauthentication WHERE replicateauthentication.id = $id";
+  //Get results
+
+    $result = $mysqli->query($query) or die($mysqli->error.__LINE__);
+        if($result = $mysqli->query($query)){
+                //Fetch object array
+                while($row = $result->fetch_assoc()) {
+                        $id = $row['id'];
+                        $dstise = $row['dstise'];
+                        $dstpolicysetid = $row['dstpolicysetid'];
+                        $authenticationname = $row['authenticationname'];
+                }
+                //Free Result set
+                $result->close();
+        }
+
+
+
+
+
+
+system("sudo -S python3 /root/ise-landscape/policyset/insert_dstpolicyid.py  $id  $dstise $dstpolicysetid '$authenticationname'");
 
 
 
